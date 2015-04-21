11162694727463eb8da7a62ae19a429a2389a44f
Adam Shimali <adam.shimali@gmail.com>
2015-04-21 17:23:16 +0100

    update test for default title text

--- test/controllers/html/SearchPagesTest.java
+++ test/controllers/html/SearchPagesTest.java
@@ -24,7 +24,7 @@ public class SearchPagesTest extends ApplicationTests {
 
         assertThat(response.getStatus()).isEqualTo(OK);
         String body = response.getBody();
-        assertThat(body).contains("The Test_register Register is the official register of test_register entries throughout the UK.");
+        assertThat(body).contains("The official register of test entries throughout the UK.");
         assertThat(body).contains("Search this register");
     }
 
