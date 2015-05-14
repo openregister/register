14d9a48736bc15681fe53678251bd7016b83be0e
omsharma <foromsharma@gmail.com>
2015-05-14 16:50:36 +0100

    Merge branch 'master' of github.com:openregister/register into select_fields_for_register
    Conflicts:
    	app/controllers/html/Utils.java

--- app/uk/gov/openregister/config/RegisterRegister.java
+++ app/uk/gov/openregister/config/RegisterRegister.java
@@ -7,31 +7,33 @@ import uk.gov.openregister.model.Cardinality;
 import uk.gov.openregister.model.Datatype;
 import uk.gov.openregister.model.Field;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class RegisterRegister extends Register {
 
-    private final List<Field> FIELDS;
+    private List<Field> FIELDS;
 
-    public RegisterRegister() {
+    @Override
+    public InitResult init() {
         String fieldsUrl = ApplicationConf.registerUrl("field", "/search?_representation=json");
         WSResponse fr = WS.client().url(fieldsUrl).execute().get(30000);
         List<String> allowedValuesForFieldsField = new ArrayList<>();
+
         if (fr.getStatus() == 200) {
             allowedValuesForFieldsField = StreamUtils.asStream(fr.asJson().elements()).map(e -> e.get("entry").get("field").textValue()).collect(Collectors.toList());
-        }
 
+        }
         FIELDS = Arrays.asList(
                 new Field("register"),
                 new Field("name"),
                 new Field("fields", "Fields", Datatype.of("list"), Cardinality.MANY, Optional.of("field"), Optional.of(allowedValuesForFieldsField)),
                 new Field("text")
         );
-    }
 
-    @Override
-    public InitResult init() {
         return InitResult.OK;
     }
 
