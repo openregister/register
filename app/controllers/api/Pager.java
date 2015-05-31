39b40ab7a476ea7e4a2a4d5d0e5fbe9bed3bbc4b
Saqib Saleem (EE) <ssaleem@equalexperts.com>
2015-05-31 14:42:34 +0100

    Added Pager which is a QueryStringBindable.
    This change better handles the pagination params.

--- app/controllers/api/Pager.java
+++ app/controllers/api/Pager.java
@@ -5,41 +5,44 @@ import play.mvc.QueryStringBindable;
 
 import java.util.Map;
 
+import static play.libs.F.Option;
+
 
 public class Pager implements QueryStringBindable<Pager> {
+    public final static Pager defaultPager = new Pager(0,30);
+
     public int page;
     public int pageSize;
+
+    public Pager() {
+    }
 
     public Pager(int p, int ps) {
         page = p;
         pageSize = ps;
     }
 
-
     @Override
     public F.Option<Pager> bind(String key, Map<String, String[]> params) {
-        if (params.containsKey(key + "_page") && params.containsKey(key + "_pageSize")) {
-            try {
-                page = Integer.parseInt(params.get(key + "_page")[0]);
-                pageSize = Integer.parseInt(params.get(key + "_pageSize")[0]);
-                return F.Option.Some(this);
-            } catch (NumberFormatException e) {
-                return F.Option.None();
-            }
-        } else {
-            return F.Option.None();
+        String _page = params.containsKey("_page") ? params.get("_page")[0] : "0";
+        String _pageSize = params.containsKey("_pageSize") ? params.get("_pageSize")[0] : "30";
+
+        try {
+            page = Integer.parseInt(_page);
+            pageSize = Integer.parseInt(_pageSize);
+            return Option.<Pager>Some(this);
+        } catch (NumberFormatException e) {
+            return Option.<Pager>None();
         }
     }
 
     @Override
     public String unbind(String key) {
-        return key + "_page=" + page + "&" + key + "_pageSize=" + pageSize;
+        return "_page=" + page + "&" + "_pageSize=" + pageSize;
     }
 
     @Override
     public String javascriptUnbind() {
-        return "function(k,v) {\n" +
-                "    return encodeURIComponent(k+'_page')+'='+v.page+'&'+encodeURIComponent(k+'_pageSize')+'='+v.pageSize;\n" +
-                "}";
+        return null;
     }
 }
