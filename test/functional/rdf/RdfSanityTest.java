fc3742c9dad1e84a1e176de026bf6bae6ebe0d78
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-06-11 16:05:19 +0100

    fix tests

--- test/functional/rdf/RdfSanityTest.java
+++ test/functional/rdf/RdfSanityTest.java
@@ -11,7 +11,7 @@ public class RdfSanityTest extends ApplicationTests {
     public static final String TEST_JSON = "{\"test-register\":\"testregisterkey\",\"name\":\"The Entry\",\"key1\": \"value1\",\"key2\": [\"A\",\"B\"]}";
     public static final String EXPECTED_HASH = "4686f89b9c983f331c7deef476fda719148de4fb";
 
-    public static final String EXPECTED_TURTLE = "@prefix field: <http://fields.openregister.org/field/>.\n" +
+    public static final String EXPECTED_TURTLE = "@prefix field: <http://field.openregister.org/field/>.\n" +
             "\n" +
             "<http://localhost:8888/hash/4686f89b9c983f331c7deef476fda719148de4fb>\n" +
             "  field:test-register <http://localhost:8888/test-register/testregisterkey> ;\n" +
@@ -41,7 +41,7 @@ public class RdfSanityTest extends ApplicationTests {
         assertThat(response.getBody()).isEqualTo(EXPECTED_TURTLE);
     }
 
-    public static final String EXPECTED_TURTLE_LIST = "@prefix field: <http://fields.openregister.org/field/>.\n" +
+    public static final String EXPECTED_TURTLE_LIST = "@prefix field: <http://field.openregister.org/field/>.\n" +
             "\n" +
             "<http://localhost:8888/hash/39837068f586ab19bcb2b5f2408b024438e75c43>\n" +
             "  field:test-register <http://localhost:8888/test-register/testregisterkey1> ;\n" +
